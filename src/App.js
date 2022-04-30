e8ce81e0104042b3e84d4db96e7919dd68465e4d
Eduardo Pérez Rosales <420090601@pcpuma.acatlan.unam.mx>
2022-04-30 14:11:18 -0500

    UpdateState
    Se añadió el state y el contador al proyecto creado.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
+/*Pérez Rosales Eduardo - 420090601
+30 de abril de 2022 */
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  //state contador inicializado en 0
+  const [contador, updateContador] = useState(0);
+  //Funcion que actualiza el contador. Se ejecuta al presionar el botón
+  const incrementar = () =>{
+    try {
+      updateContador(contador+1);
+    } catch (error) {
+      alert("Algo salió mal. Intenta de nuevo.")
+    }
+  }
+  //Retorno de la función principal App
+  //Al hacer clic en el botón se ejecuta la funcion Incrementar
   return (
     <div className="App">
       <header className="App-header">
+        <p>Actividad 8</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Contador: {contador}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={incrementar}>Incrementar</button>
+        <p>Hecho por: Pérez Rosales Eduardo - 420090601</p>
       </header>
     </div>
   );
