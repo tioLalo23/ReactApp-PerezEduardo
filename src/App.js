c200edfc663afc194513a3487e320ec845bf765b
Eduardo Pérez Rosales <420090601@pcpuma.acatlan.unam.mx>
2022-05-10 14:25:47 -0500

    Form
    Se añadió el formulario y las funciones para actualizarlo. Funciona con un json único asociado al CP 72760.
    Falta conectar al API.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,88 @@
 /*Pérez Rosales Eduardo - 420090601
 30 de abril de 2022 */
-import logo from './logo.svg';
 import './App.css';
-import React, {useState} from 'react';
+import resultado from './resultado.json';
+
+
 
 function App() {
-  //state contador inicializado en 0
-  const [contador, updateContador] = useState(0);
-  //Funcion que actualiza el contador. Se ejecuta al presionar el botón
-  const incrementar = () =>{
-    try {
-      updateContador(contador+1);
-    } catch (error) {
-      alert("Algo salió mal. Intenta de nuevo.")
+  
+  //const axios = require('axios').default;
+
+  const peticion = (codigoPostal) => {
+         
+  }
+
+  const validar = () => {
+    var objeto = document.getElementById("cp");
+    if(objeto.value.length >5){
+      objeto.value = objeto.value.substring(0,5);
     }
+    var cp = document.getElementById("cp").value;
+    var estado = document.getElementById("estado");
+    var municipio = document.getElementById("municipio");
+    var colonia = document.getElementById("colonia");
+    estado.innerHTML = '<option>Seleccione</option>';
+    municipio.innerHTML = '<option>Seleccione</option>';
+    colonia.setAttribute("disabled", "true");
+    colonia.innerHTML = '<option>Seleccione</option>';
+    if(cp.length==5){
+      console.log(cp);
+      estado.innerHTML+='<option value='+resultado[0].idEstado+' selected> '+resultado[0].estado+'</option>';
+      municipio.innerHTML+='<option value='+resultado[0].idMun+' selected> '+resultado[0].municipio+'</option>';
+      colonia.removeAttribute("disabled");
+      for(let i=0; i<resultado.length; i++){
+        colonia.innerHTML +='<option value='+resultado[i].idColonia+'> '+resultado[i].colonia+'</option>';
+      }
+    }
+      /*var resultados = JSON.parse(resultado[0]);
+      console.log(resultados);*/
+      //Se consume el API
+      /*axios.post('http://api.masksoftco.mx/direcciones/codigo-postal', {
+          cp: codigoPostal,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });*/
+    
+
   }
   //Retorno de la función principal App
   //Al hacer clic en el botón se ejecuta la funcion Incrementar
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Actividad 8</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Contador: {contador}
-        </p>
-        <button onClick={incrementar}>Incrementar</button>
-        <p>Hecho por: Pérez Rosales Eduardo - 420090601</p>
-      </header>
+      <div class="container">
+        <h1>Actividad 9</h1>
+        <div class="row">
+          <div class="col-md-3">
+            <p>Ingresa tu código postal</p>
+              <input type="number" id='cp' max={99999} onChange={validar}>
+              </input>
+          </div>
+          <div class="col-md-3">
+            <p>Ingresa tu estado</p>
+              <select class="form-select" aria-label="Default select example" id='estado' disabled>
+                <option>Seleccione</option>
+              </select>
+          </div>
+          <div class="col-md-3">
+            <p>Ingresa tu municipio</p>
+              <select class="form-select" aria-label="Default select example" id='municipio' disabled>
+                <option>Seleccione</option>
+              </select>
+          </div>
+          <div class="col-md-3">
+            <p>Ingresa tu colonia</p>
+              <select class="form-select" aria-label="Default select example" id='colonia' disabled>
+                <option selected>Seleccione</option>
+              </select>
+          </div>
+        </div>  
+      </div>
+      
     </div>
   );
 }
