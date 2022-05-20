3ce685cd9585dd4c2701e55bfd6b9bbb5ba82fe7
Eduardo Pérez Rosales <420090601@pcpuma.acatlan.unam.mx>
2022-05-20 14:36:12 -0500

    Actividad
    Se consume el API y se muestran los resultados al dar clic al botón. Se debe configurar el acceso al API.

--- src/App.js
+++ src/App.js
@@ -1,100 +1,85 @@
 /*Pérez Rosales Eduardo - 420090601
 30 de abril de 2022 */
 import './App.css';
-import swal from 'sweetalert';
-//import resultado from './resultado.json';
+import { useState } from 'react';
+
 const axios = require('axios').default;
 
 
 function App() {
-  
+  const [resultado, setResultado] = useState([]);
   const validar = async () => {
     try {
-      //Inicia los inputs en sus estados por default
-      var objeto = document.getElementById("cp");
-      var cp = document.getElementById("cp").value;
-      var estado = document.getElementById("estado");
-      var municipio = document.getElementById("municipio");
-      var colonia = document.getElementById("colonia");
-      estado.innerHTML = '<option>Seleccione</option>';
-      municipio.innerHTML = '<option>Seleccione</option>';
-      colonia.setAttribute("disabled", "true");
-      colonia.innerHTML = '<option>Seleccione</option>';
-      //Recorta el numero a 5 cifras
-      if(objeto.value.length >5){
-        objeto.value = objeto.value.substring(0,5);
-      }
-      //Cuando tiene 5 cifras, evalúa el cp
-      if(cp.length==5){
-        console.log(cp);
-        //Si es negativo, escapa de la función
-        if(cp<=0){
-          swal("Número inválido","Ingrese un entero positivo de 5 cifras", "error");
-          return 0;
-        }
-        //Crea un nuevo formdata y lo envía al API
-        let formData = new FormData();
-        formData.append("cp", cp);
-        const url = "http://api.masksoftco.mx/direcciones/codigo-postal";
-        let res = await axios({
-            url,
-            method: 'POST',
-            dataType: 'json',
-            ContentType: 'application/json',
-            data: formData
-        });
-        let resultado = res.data;
-        //Evalua si existe el cp
-        if(resultado==0){
-          swal("Error","El codigo postal no existe","error");
-        }
-        else {
-          //Si existe, agrega sus valores a los input
-          estado.innerHTML+='<option value='+resultado[0].idEstado+' selected> '+resultado[0].estado+'</option>';
-          municipio.innerHTML+='<option value='+resultado[0].idMun+' selected> '+resultado[0].municipio+'</option>';
-          colonia.removeAttribute("disabled");
-          for(let i=0; i<resultado.length; i++){
-              colonia.innerHTML +='<option value='+resultado[i].idColonia+'> '+resultado[i].colonia+'</option>';
-          }
-        }
-        
-      }
-      
+      const url = "https://apigrupogr.com/grapi/clientes/lista-clientes";
+      let res = await axios({
+          url,
+          method: 'GET',
+          //dataType: 'json',
+          //ContentType: 'application/json',
+      });
+      setResultado(res.data.clients);
+      console.log(resultado);
     } catch (error) {
       //Imprime el error en caso de haber
       console.log(error);
     }
-    
   }
   //Retorno de la función principal App
   //Al hacer clic en el botón se ejecuta la funcion Incrementar
   return (
     <div className="App">
       <div class="container">
-        <h1 class="py-3">Actividad 9</h1>
+        <h1 class="py-3">Actividad 10</h1>
         <div class="row">
-          <div class="col-md-3">
-            <p>Ingresa tu código postal</p>
-              <input type="number" id='cp' max={99999} onChange={validar}>
-              </input>
+          <div class="col-md-1 align-middle">
+            <p>Clic aqui para consultar</p>
+              <input type="button" id='boton' onClick={validar} value="Consultar"></input>
           </div>
-          <div class="col-md-3">
-            <p>Ingresa tu estado</p>
-              <select class="form-select" aria-label="Default select example" id='estado' disabled>
-                <option>Seleccione</option>
-              </select>
-          </div>
-          <div class="col-md-3">
-            <p>Ingresa tu municipio</p>
-              <select class="form-select" aria-label="Default select example" id='municipio' disabled>
-                <option>Seleccione</option>
-              </select>
-          </div>
-          <div class="col-md-3">
-            <p>Ingresa tu colonia</p>
-              <select class="form-select" aria-label="Default select example" id='colonia' disabled>
-                <option selected>Seleccione</option>
-              </select>
+          <div class="col-md-11">
+            <div class="row fw-bold">
+              <div class="col-md-2">
+                <p>id</p>
+              </div>
+              <div class="col-md-2">
+                <p>Nombre</p>
+              </div>
+              <div class="col-md-2">
+                <p>Descripción</p>
+              </div>
+              <div class="col-md-2">
+                <p>Correo</p>
+              </div>
+              <div class="col-md-2">
+                <p>Regimen fiscal</p>
+              </div>
+              <div class="col-md-2">
+                <p>RFC</p>
+              </div>
+            </div>
+            {
+              resultado.map((data) => (
+                <div class="row" key={'cliente-'+ data.id_cliente}>
+                  <div class="col-md-2">
+                    {data.id_cliente}
+                  </div>
+                  <div class="col-md-2">
+                    {data.nombre}
+                  </div>
+                  <div class="col-md-2">
+                    {data.descripcion}
+                  </div>
+                  <div class="col-md-2">
+                    {data.correo}
+                  </div>
+                  <div class="col-md-2">
+                    {data.reg_fiscal}
+                  </div>
+                  <div class="col-md-2">
+                    {data.rfc}
+                  </div>
+                </div>
+              ))
+            }          
           </div>
         </div>
         <div class="row py-5">
